4c9a7307a5166658a17ff25a943686d1426800da
juliema <julieallen34@gmail.com>
2013-06-15 20:37:02 -0500

    Update runtblastn.pl

--- runtblastn.pl
+++ runtblastn.pl
@@ -2,17 +2,10 @@
 
 $numtargets=3000;
 
+### I donnt think the numtargests worked 
+
+
 $path="/Users/juliema/Documents/Projects/TRAMiEvoBio/P_schaefBlastDB";
 
-system "ls -l *.fas >filenames";
+system "tblastn -db $path/s_3_1_sequence_pediculus_blast_database -outfmt 7 -max_target_seqs $numtargets -query Pediculus_hum.peptides.fas -out tblastn.out";
 
-open FH, "<filenames";
-while (<FH>)
-{
-    if (/(\S+).fas/ && ! /Ped_all/)
-    {
-  $query = $1;
-	print "$query\n";
-	system "tblastn -db $path/s_3_1_sequence_pediculus_blast_database -outfmt 7 -max_target_seqs $numtargets -query $query.fas -out $query.tblastn.max$numtargets.out ";
-    }
-}
