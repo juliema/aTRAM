5bdcf5f027badc1fd1e6a4b74cf5943838219df4
daisieh_local <daisieh@gmail.com>
2013-08-20 17:31:01 -0700

    added a bit of wrapper so that it outputs to $1.sorted.fasta

--- 3.5-sort_fasta.sh
+++ 3.5-sort_fasta.sh
@@ -1,4 +1,6 @@
 # sorts an inputted fasta file of Illumina multiplexed short reads, such as the one from 3-getpairedend.pl.
 # execute with the shell of your choice, i.e. "bash file.fasta > sorted_file.fasta"
 
-gawk -e '/#/ {sub(/lcl\|/,""); print $1 "," $2 $3 }' RS=">" $1 | sort | gawk -e '/#/ {print ">" $1 "\n" $2}' FS=","
+infile=$1
+outfile=$1.sorted.fasta
+gawk -e '/#/ {sub(/lcl\|/,""); print $1 "," $2 $3 }' RS=">" $infile | sort | gawk -e '/#/ {print ">" $1 "\n" $2}' FS="," > $outfile
