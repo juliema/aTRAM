feb2bb902a112ae67c77e1c7bcd9fc18700fbaa9
daisieh_local <daisieh@gmail.com>
2013-08-23 14:24:23 -0700

    search for paired seqs in fasta files

---
+++ 2.5-sequenceretrieval.pl
@@ -0,0 +1,26 @@
+use strict;
+
+my $seq_names = shift;
+my $fastafile_1 = shift;
+my $fastafile_2 = shift;
+my $outfile = "$seq_names.fasta";
+
+open LIST_FH, "<", "$seq_names";
+open FA1_FH, "<", "$fastafile_1";
+open FA2_FH, "<", "$fastafile_2";
+open OUT_FH, ">", "$outfile";
+
+my $curr_name = readline LIST_FH;
+
+while ($curr_name) {
+	my $fa_seq1 = (readline FA1_FH) . (readline FA1_FH);
+	my $fa_seq2 = (readline FA2_FH) . (readline FA2_FH);
+
+	if ($fa_seq1 eq "") { last; }
+	if ($fa_seq2 eq "") { last; }
+
+	if ($fa_seq1 =~ /$curr_name/) {
+		print OUT_FH "$fa_seq1$fa_seq2";
+		$curr_name = readline LIST_FH;
+	}
+}
