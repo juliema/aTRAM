d77e9ee38d8940824cc22bac00e0b70cc2dacc00
daisieh_local <daisieh@gmail.com>
2013-08-26 14:40:29 -0700

    took out debug print statements

--- 2.5-sequenceretrieval.pl
+++ 2.5-sequenceretrieval.pl
@@ -7,7 +7,7 @@ my $seq_blast = shift;
 my $seq_names = "$seq_blast.sorted";
 my $outfile = "$seq_names.fasta";
 system ("gawk '{sub(\"/1\",\"\");print \$2;}' $seq_blast | sort | uniq -u > $seq_names");
-print "made file $seq_names\n";
+# print "made file $seq_names\n";
 
 open LIST_FH, "<", "$seq_names";
 open FA1_FH, "<", "$fastafile_1";
@@ -25,7 +25,7 @@ while ($curr_name) {
 	if ($fa_seq2 eq "") { last; }
 
 	if ($fa_seq1 =~ /$curr_name/) {
-		print "found $curr_name\n";
+# 		print "found $curr_name\n";
 		print OUT_FH "$fa_seq1$fa_seq2";
 		$curr_name = readline LIST_FH;
 	}
