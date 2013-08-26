8997c7527769e18f41d85e34ec1558b32b3de5b0
daisieh_local <daisieh@gmail.com>
2013-08-26 14:33:42 -0700

    test of pipeline with multiple iterations

---
+++ pipeline.pl
@@ -0,0 +1,15 @@
+use strict;
+
+my $short_read_archive = shift;
+
+#initially, run the pipeline on the inputted gene:
+my $search_fasta = shift;
+
+for (my $i=0; $i<5; $i++) {
+	system("blastn -db $short_read_archive.db -query search_fasta -outfmt 6 -num_threads 8 -out $short_read_archive.blast.$i");
+	system ("perl ~/TRAM/2.5-sequenceretrieval.pl $short_read_archive.1.fasta $short_read_archive.2.fasta $short_read_archive.blast.$i");
+	system ("velveth $short_read_archive.velvet 31 -fasta -shortPaired $short_read_archive.fasta.sorted.fasta");
+	system ("velvetg $short_read_archive.velvet -ins_length 300 -exp_cov 30 -min_contig_lgth 200");
+	$search_fasta = "$short_read_archive.$1.contigs.fa";
+	system ("mv $short_read_archive.velvet/contigs.fa $search_fasta");
+}
