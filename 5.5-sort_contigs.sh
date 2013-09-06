128e852f51ed9912ad5f355d9ac7fd4205c6239f
daisieh_local <daisieh@gmail.com>
2013-09-06 16:53:47 -0700

    update to use true length, not just info in fasta header.

--- 5.5-sort_contigs.sh
+++ 5.5-sort_contigs.sh
@@ -3,6 +3,6 @@
 
 infile=$1
 outfile=$1.sorted.tab
-gawk '{if ($0 - /^$/) next; s = ""; for (i=2;i<=NF;i++) s = s$i; print $1","s}' RS=">" $infile | gawk '{s = ""; for (i=1;i<=NF;i++) s = s"_"$i;print $4 "," s}' FS="_" | sort -n -r | gawk '{sub ("_","",$2); print $1 "\t" $2 "\t" $3}' FS="," > $outfile
+gawk '{if ($0 - /^$/) next; s = ""; for (i=2;i<=NF;i++) s = s$i; print length(s)","$1","s}' RS=">" $infile | sort -n -r | gawk '{ print $1 "\t" $2 "\t" $3}' FS="," > $outfile
 
 # gawk '{sub(/[[:digit:]]+_/,"",$1); print ">" $1 "\n" $2}' FS="," > $outfile
