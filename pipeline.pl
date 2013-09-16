5c77baf8e92f8a4af79c9b3cea49c300671849da
daisieh_local <daisieh@gmail.com>
2013-09-15 18:24:34 -0700

    Updated POD

--- pipeline.pl
+++ pipeline.pl
@@ -139,13 +139,20 @@ pipeline.pl
 
 =head1 SYNOPSIS
 
-pipeline.pl -reads -target [-ins_length]
+pipeline.pl -reads shortreadfile -target target.fasta [-ins_length int] [-exp_coverage int] [-iterations int] [-start_iteration int] [-log_file filename] [-use_ends] [-output filename]
 
 =head1 OPTIONS
 
   -reads:     		short read archive (already run through 0-prepare_files.pl).
   -target:          fasta file with sequences of interest.
-  -ins_length:	    optional: if specified, the size of the fragments used in the short-read library (default 300).
+  -output:	        optional: the prefix for the pipeline's output files (default name is the same as -reads).
+  -ins_length:	    optional: the size of the fragments used in the short-read library (default 300).
+  -exp_coverage:    optional: the expected coverage of the region for velvetg (default 30).
+  -iterations:      optional: the number of pipeline iterations (default 5).
+  -start_iteration: optional: if resuming from previous run, which iteration number to start from (default 0).
+  -log_file:        optional: a file to store output of the pipeline.
+  -use_ends:        optional: if this flag is present, use the first and last $ins_length of long contigs in the search.
+
 
 =head1 DESCRIPTION
 
