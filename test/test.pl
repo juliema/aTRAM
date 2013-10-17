c75cb9086cfe7a3e6cb3a0594e0a579d378cd13c
daisieh_local <daisieh@gmail.com>
2013-10-16 18:35:01 -0700

    took out checks for modules, as we don't use any anymore

--- test/test.pl
+++ test/test.pl
@@ -1,84 +1,88 @@
 #!/usr/bin/perl
 use strict;
-use ExtUtils::Installed;
+# use ExtUtils::Installed;
 use File::Spec;
-# use IPC::System::Simple qw(run system capture EXIT_ANY);
+use File::Basename;
 
 
-my @req_modules = qw(IPC::System::Simple);
-
-@req_modules = sort @req_modules;
+# my @req_modules = qw(IPC::System::Simple);
+#
+# @req_modules = sort @req_modules;
 my $i = 1;
+print "==== sTRAM checklist ====\n\n";
 
-print $i++ . ". Checking for required modules...\n";
-my $installed_modules = ExtUtils::Installed->new;
-# my @modules = $installed_modules->modules();
+# print $i++ . ". Checking for required modules...\n";
+# my $installed_modules = ExtUtils::Installed->new;
 
-foreach my $mod (@req_modules) {
-	my $res = $installed_modules->validate($mod);
-}
+# foreach my $mod (@req_modules) {
+# 	my $res = $installed_modules->validate($mod);
+# }
 
 print $i++ .". Checking for required software...\n";
-my @req_software = qw(velveth velvetg blastn makeblastdb);
+my @req_software = qw(velveth velvetg blastn makeblastdb gawk);
 
 my $sw_ready = 1;
 foreach my $sw (@req_software) {
-	open my $saveout, ">&STDOUT";
-	open STDOUT, '>', File::Spec->devnull();
+	my $result = system_call("$sw 2>&1 1>/dev/null");
 
-	my $result = system("$sw 2>&1 1>/dev/null");
-	#Restore STDOUT
-	open STDOUT, ">&", $saveout;
-
-	$result = ($result >> 8);
 	if ($result == 127) {
-		print "   ...package $sw is not installed.\n";
+		print "   ...$sw is not installed.\n";
 		$sw_ready = 0;
-	# 	my $userpath = <STDIN>;
-	# 	if ($userpath =~ /[qQeE]\n/) {
-	# 		print "quit";
-	# 	} else {
-	# 	print "  you said $userpath";
-	# 	}
 	} else {
-		print "   ...package $sw is present.\n";
+		print "   ...$sw is present.\n";
 	}
 }
 
 if ($sw_ready == 0) {
-	die "You need to install some software before you can run sTRAM.\n";
+	print "You need to install some software before you can run sTRAM. Continue checks? [Y/n]\n";
+	my $userpath = <STDIN>;
+	if ($userpath =~ /[nN]\n/) {
+		exit;
+	}
 }
 
 print $i++ .". Checking that prepare_files works correctly...\n";
 
-open my $saveout, ">&STDOUT";
-open STDOUT, '>', File::Spec->devnull();
-system ("cp test_good.fastq test.fastq");
-eval {
-	system ("perl ../0-prepare_files.pl test.fastq");
-};
-open STDOUT, ">&", $saveout;
+my $executing_path = dirname(__FILE__);
 
+unless (system_call ("cp $executing_path/test_good.fastq test_inst.fastq") == 0) {
+	die "Couldn't find test_good.fastq";
+}
 
-if ($@) {
+my $result = system_call ("perl ../0-prepare_files.pl test_inst.fastq");
+if ($result == 1) {
 	print "Test failed. Please contact the developers with details of this failure at https://github.com/juliema/TRAM/issues.\n";
 	exit;
 }
-# print $i++ .". Checking a defective file...\n";
-#
-# open my $saveout, ">&STDOUT";
-# open my $saveerr, ">&STDERR";
-# open STDERR, '>', File::Spec->devnull();
-# system ("cp test_bad.fasta test.fasta");
-# eval {
-# 	system ("perl ../0-prepare_files.pl test_bad.fasta");
-# };
-# open STDOUT, ">&", $saveout;
-# open STDERR, ">&", $saveerr;
-#
-# if ($@) {
-# 	print "...0-prepare_files passes tests.\n";
-# }
-#
-#
+
+print $i++ .". Checking a defective file...\n";
+unless (system_call ("cp $executing_path/test_bad.fasta test_inst.fasta") == 0) {
+	die "Couldn't find test_bad.fasta";
+}
+
+my $result = system_call ("perl ../0-prepare_files.pl test_inst.fasta");
+if ($result == 0) {
+	print "Test failed. Please contact the developers with details of this failure at https://github.com/juliema/TRAM/issues.\n";
+	exit;
+}
+
 print "Looks good! You are ready to sTRAM it up!\n";
+system_call("rm test_inst.*");
+
+sub system_call {
+	my $cmd = shift;
+
+	open my $saveout, ">&STDOUT";
+	open my $saveerr, ">&STDERR";
+	open STDOUT, '>', File::Spec->devnull();
+	open STDERR, '>', File::Spec->devnull();
+
+	my $exit_val = eval {
+		system ($cmd);
+	} >> 8;
+
+	open STDOUT, ">&", $saveout;
+	open STDERR, ">&", $saveerr;
+
+	return $exit_val;
+}
