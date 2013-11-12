a8183a68628ad93c01c8e5c4fece3a4cad05f5ee
daisieh_local <daisieh@gmail.com>
2013-11-12 15:38:35 -0800

    fixing package details

--- lib/Velvet.pm
+++ lib/Velvet.pm
@@ -8,34 +8,35 @@ require Subfunctions;
 	 	# what the assembly parameters are. (pass this in as a hash)
 	# Assembler modules should return a file name for the resulting contigs.
 
+package Velvet;
 
 sub assembler {
+	my $self = shift;
 	my $short_read_file = shift;
 	my $params = shift;
 	my $log_fh = shift;
 
 	my ($kmer, $tempdir, $longreads, $ins_length, $exp_cov, $min_contig_len) = 0;
-	if (ref $params =~ /HASH/) {
-		if ($params->{"kmer"}) {
+	if ((ref $params) =~ /HASH/) {
+        if (exists $params->{"kmer"}) {
 			$kmer = $params->{"kmer"};
 		}
-		if ($params->{"tempdir"}) {
+		if (exists $params->{"tempdir"}) {
 			$tempdir = $params->{"tempdir"};
 		}
-		if ($params->{"longreads"}) {
+		if (exists $params->{"longreads"}) {
 			$longreads = $params->{"longreads"};
 		}
-		if ($params->{"ins_length"}) {
+		if (exists $params->{"ins_length"}) {
 			$ins_length = $params->{"ins_length"};
 		}
-		if ($params->{"exp_cov"}) {
+		if (exists $params->{"exp_cov"}) {
 			$exp_cov = $params->{"exp_cov"};
 		}
-		if ($params->{"min_contig_len"}) {
+		if (exists $params->{"min_contig_len"}) {
 			$min_contig_len = $params->{"min_contig_len"};
 		}
 	}
-
 	# using velvet
 	print "\tassembling with Velvet...\n";
 	if ($longreads != 0) {
