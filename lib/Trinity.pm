69b1a8d650c72869f73c1049148a5969954ae562
daisieh_local <daisieh@gmail.com>
2013-12-08 21:47:00 -0800

    fixing rename_contigs

--- lib/Trinity.pm
+++ lib/Trinity.pm
@@ -66,33 +66,14 @@ sub rename_contigs {
 	open OUTFH, ">", $renamefile;
 	while (my $line = readline FH) {
 		if ($line =~ /^>/) {
-			#NODE_41_length_2668_cov_4.901050
-			$line =~ s/^>NODE_(\d+)_length_(\d+)_cov_(\d+\.\d).*$/>$prefix$1_len_$2_cov_$3/;
+			# >comp0_c0_seq1 len=716 path=[1070:0-715]
+			# >comp0_c1_seq1 len=3433 path=[4485:0-3432]
+			# >comp1_c0_seq1 len=1572 path=[2936:0-1571]
+			# >comp2_c0_seq1 len=4637 path=[7895:0-4636]
+			$line =~ s/^>comp(\d+)_(c\d+)_seq. len=(\d+).*$/>$prefix$1$2_len_$3/;
 		}
 		print OUTFH $line;
 	}
 }
-
-sub system_call {
-	my $cmd = shift;
-	my $log_fh = shift;
-
-	unless ($log_fh) {
-		$log_fh = &STDOUT;
-	}
-
-	print $log_fh ("\t$cmd\n");
-	my $exit_val = eval {
-		system ($cmd);
-	};
-
-	if ($exit_val != 0) {
-		print "System call \"$cmd\" exited with $exit_val\n";
-		exit;
-	}
-
-	return $exit_val;
-}
-
 
 return 1;
