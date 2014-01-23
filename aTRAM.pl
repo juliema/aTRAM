93c2b5d31f0321037699942801560e14336b5bcd
daisieh_local <daisieh@gmail.com>
2014-01-22 18:49:52 -0800

    renamed $short_read_archive to $atram_db, $intermediate to $temp_name, fixed $save_temp flag.

--- aTRAM.pl
+++ aTRAM.pl
@@ -24,7 +24,7 @@ if (@ARGV == 0) {
 my $runline = "running " . basename($0) . " " . join (" ", @ARGV) . "\n";
 
 #required: input file names
-my $short_read_archive = 0;
+my $atram_db = 0;
 my $target_fasta = 0;
 
 #optional parameters:
@@ -33,7 +33,7 @@ my $log_file = "";
 my $output_file = "";
 my $shards = 0;
 my $fraction = 1;
-my $intermediate = 0;
+my $temp_name = 0;
 my $save_temp = 0;
 my $complete = 0;
 my $protflag = 0;
@@ -51,7 +51,7 @@ my $max_target_seqs = 100000000;
 my $assembler = "Velvet";
 my $kmer = 31;
 
-GetOptions ('reads=s' => \$short_read_archive,
+GetOptions ('reads|sra|database|db=s' => \$atram_db,
             'target=s' => \$target_fasta,
             'assembler=s' => \$assembler,
             'start_iteration=i' => \$start_iter,
@@ -61,7 +61,7 @@ GetOptions ('reads=s' => \$short_read_archive,
             'output=s' => \$output_file,
             'protein' => \$protflag,
             'max_processes|processes=i' => \$max_processes,
-            'tempfiles=s' => \$save_temp,
+            'tempfiles=s' => \$temp_name,
             'debug' => \$debug,
             'complete' => \$complete,
             'insert_length|ins_length=i' => \$ins_length,
@@ -80,26 +80,27 @@ if ($debug) {
 	set_debug(1);
 }
 
-unless($short_read_archive and $target_fasta) {
+unless($atram_db and $target_fasta) {
     pod2usage(-msg => "Must specify a short read archive (that has already been prepared with makelibrary.pl) and a target gene in fasta form.");
 }
 
 # check to make sure that the specified short read archive exists:
-unless ((-e "$short_read_archive.0.1.fasta") && (-e "$short_read_archive.0.2.fasta")) {
+unless ((-e "$atram_db.0.1.fasta") && (-e "$atram_db.0.2.fasta")) {
 	pod2usage(-msg => "Short read archive does not seem to be in the format made by makelibrary.pl. Did you specify the name correctly?");
 }
 
 
 if ($output_file eq "") {
-    $output_file = $short_read_archive;
+    $output_file = $atram_db;
 }
 if ($log_file eq "") {
 	$log_file = "$output_file.log";
 }
-if ($save_temp == 0) {
-    $intermediate = $output_file;
+if ($temp_name eq "") {
+    $temp_name = $output_file;
+    $save_temp = 0;
 } else {
-    $intermediate = $save_temp;
+	$save_temp = 1;
 }
 
 my $log_fh;
@@ -112,7 +113,7 @@ my $executing_path = dirname(__FILE__);
 my $cmd;
 
 # set up the number of partial libraries we'll be using.
-my $total_shards_available = get_total_shards("$short_read_archive");
+my $total_shards_available = get_total_shards("$atram_db");
 my $max_shard = get_max_shard();
 
 $shards = int ($total_shards_available * $fraction);
@@ -122,7 +123,7 @@ if ($shards == 0) {
 	$shards = 1;
 }
 
-unless ((-e "$short_read_archive.$max_shard.1.fasta") && (-e "$short_read_archive.$max_shard.2.fasta")) {
+unless ((-e "$atram_db.$max_shard.1.fasta") && (-e "$atram_db.$max_shard.2.fasta")) {
 	pod2usage(-msg => "aTRAM database was improperly formatted.");
 }
 
@@ -220,7 +221,7 @@ if ($protein == 1) {
 
 if ($start_iter > 1) {
 	my $x = $start_iter-1;
-	$search_fasta = "$intermediate.".($start_iter-1).".contigs.fasta";
+	$search_fasta = "$temp_name.".($start_iter-1).".contigs.fasta";
 }
 
 # writing the header line for the results file
@@ -235,22 +236,23 @@ for (my $i=$start_iter; $i<=$iterations; $i++) {
 	print ("iteration $i starting...\n");
 	print $log_fh ("iteration $i starting...\n");
 
-	my $sra = "$short_read_archive";
-	my $current_partial_file = "$intermediate.blast.$i";
-	my @partialfiles = ();
+	my @shardfiles = ();
 	my @pids = ();
 
-	# we are multithreading:
+	################################################################
+	# MAP steps:
+	################################################################
+
+	#   blast search in each shard:
 	print "\tblasting short reads...\n";
-	for (my $p=0; $p<$shards; $p++) {
-		$sra = "$short_read_archive.$p";
-		$current_partial_file = "$intermediate.blast.$i.$p";
-		push @partialfiles, $current_partial_file;
+	for (my $s=0; $s<$shards; $s++) {
+		my $current_shard = "$temp_name.blast.$i.$s";
+		push @shardfiles, $current_shard;
 		# 1. blast to find any short reads that match the target.
 		if (($protein == 1) && ($i == 1)) {
-			push @pids, fork_cmd ("tblastn -max_target_seqs $max_target_seqs -db $sra.db -query $search_fasta -outfmt '6 sseqid' -out $current_partial_file");
+			push @pids, fork_cmd ("tblastn -max_target_seqs $max_target_seqs -db $atram_db.$s.db -query $search_fasta -outfmt '6 sseqid' -out $current_shard");
 		} else {
-			push @pids, fork_cmd ("blastn -task blastn -evalue $evalue -max_target_seqs $max_target_seqs -db $sra.db -query $search_fasta -outfmt '6 sseqid' -out $current_partial_file");
+			push @pids, fork_cmd ("blastn -task blastn -evalue $evalue -max_target_seqs $max_target_seqs -db $atram_db.$s.db -query $search_fasta -outfmt '6 sseqid' -out $current_shard");
 		}
 		if (($max_processes > 0) && (@pids >= ($max_processes - 1))) {
 			# don't spawn off too many threads at once.
@@ -259,12 +261,12 @@ for (my $i=$start_iter; $i<=$iterations; $i++) {
 	}
 	wait_for_forks(\@pids);
 
+	#   find the blast-hit paired sequences in each shard:
 	print "\tgetting paired ends...\n";
-	for (my $p=0; $p<$shards; $p++) {
-		$sra = "$short_read_archive.$p";
-		$current_partial_file = "$intermediate.blast.$i.$p";
+	for (my $s=0; $s<$shards; $s++) {
+		my $current_shard = "$temp_name.blast.$i.$s";
 		# 2 and 3. find the paired end of all of the blast hits.
-		push @pids, fork_pair_retrieval("$sra.#.fasta", "$current_partial_file", "$current_partial_file.fasta");
+		push @pids, fork_pair_retrieval("$atram_db.$s.#.fasta", "$current_shard", "$current_shard.fasta");
 		if (($max_processes > 0) && (@pids >= ($max_processes - 1))) {
 			# don't spawn off too many threads at once.
 			wait_for_forks(\@pids);
@@ -272,33 +274,39 @@ for (my $i=$start_iter; $i<=$iterations; $i++) {
 	}
 	wait_for_forks(\@pids);
 
-	# now we need to piece all of the partial files back together.
-	my $fastafiles = join (" ", map {$_ . ".fasta"} @partialfiles);
-	system_call ("cat $fastafiles > $intermediate.$i.blast.fasta");
+	################################################################
+	# REDUCE steps:
+	################################################################
+
+	#   join the fasta results of each shard into a single fasta file:
+	my $fastafiles = join (" ", map {$_ . ".fasta"} @shardfiles);
+	system_call ("cat $fastafiles > $temp_name.$i.blast.fasta");
+
+	#   remove shards' fasta results
 	system_call ("rm $fastafiles");
 
-	# remove intermediate blast results
-	my $readfiles = join (" ", @partialfiles);
+	#   remove shards' blast results
+	my $readfiles = join (" ", @shardfiles);
 	system_call ("rm $readfiles");
 
 	# SHUTDOWN CHECK: did we not find any reads? Go ahead and quit.
-	if ((-s "$intermediate.$i.blast.fasta") == 0) {
+	if ((-s "$temp_name.$i.blast.fasta") == 0) {
 		print "No similar reads were found.\n";
 		last;
 	}
 	my $contigs_file = "";
 	if ($save_temp) {
-		$contigs_file = "$intermediate.$i.contigs.fasta";
+		$contigs_file = "$temp_name.$i.contigs.fasta";
 	} else {
 		(undef, $contigs_file) = tempfile(UNLINK => 1);
 	}
 
+	#   assemble the sequences:
 	print "\tassembling pairs with $assembler...\n";
-	# 4. assemble the reads...
 	load "$assembler";
 
 	my $assembly_params = { 'kmer' => $kmer,
-							'tempdir' => "$intermediate.$assembler",
+							'tempdir' => "$temp_name.$assembler",
 							'ins_length' => $ins_length,
 							'exp_cov' => $exp_cov,
 							'min_contig_len' => 200,
@@ -309,16 +317,16 @@ for (my $i=$start_iter; $i<=$iterations; $i++) {
 		$assembly_params->{'longreads'} = $search_fasta;
 	}
 
-	my $assembled_contig_file = "$assembler"->assembler ("$intermediate.$i.blast.fasta", $assembly_params, $log_fh);
+	my $assembled_contig_file = "$assembler"->assembler ("$temp_name.$i.blast.fasta", $assembly_params, $log_fh);
 	"$assembler"->rename_contigs($assembled_contig_file, $contigs_file);
 
 
-	# 5. now we filter out the contigs to look for just the best ones.
+	# filter out the contigs to look for just the best ones:
 	print "\tfiltering contigs...\n";
 
 	my $blast_file = "";
-	if ($save_temp) {
-		$blast_file = "$intermediate.$i.blast";
+	if ($temp_name) {
+		$blast_file = "$temp_name.$i.blast";
 	} else {
 		(undef, $blast_file) = tempfile(UNLINK => 1);
 	}
@@ -358,7 +366,7 @@ for (my $i=$start_iter; $i<=$iterations; $i++) {
 
 	# we'll use the resulting contigs as the query for the next iteration.
 	if ($save_temp) {
-		$search_fasta = "$intermediate.$i.contigs.fasta";
+		$search_fasta = "$temp_name.$i.contigs.fasta";
 	} else {
 		(undef, $search_fasta) = tempfile(UNLINK => 1);
 	}
