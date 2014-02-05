e68d1877faf9e1057ada8c15a4e60f0c6866b9f9
daisieh_local <daisieh@gmail.com>
2014-02-05 15:48:03 -0800

    export all of the subfunctions.

--- lib/Subfunctions.pm
+++ lib/Subfunctions.pm
@@ -12,7 +12,7 @@ BEGIN {
 	# Inherit from Exporter to export functions and variables
 	our @ISA         = qw(Exporter);
 	# Functions and variables which are exported by default
-	our @EXPORT      = qw();
+	our @EXPORT      = qw(timestamp exit_with_msg fork_cmd wait_for_forks printlog system_call debug set_debug set_log parsefasta sortfasta flattenfasta make_hit_matrix process_hit_matrix set_multiplier get_multiplier map_to_shard set_total_shards get_total_shards get_max_shard is_protein percentcoverage split_seq);
 	# Functions and variables which can be optionally exported
 	our @EXPORT_OK   = qw();
 }
