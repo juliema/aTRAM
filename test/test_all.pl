bf7616209d43abd8bd6db5c1421545107ac3bf1c
daisieh_local <daisieh@gmail.com>
2014-02-12 01:12:13 -0800

    use tempdir.

--- test/test_all.pl
+++ test/test_all.pl
@@ -2,33 +2,63 @@
 use strict;
 # use File::Spec;
 use FindBin;
+use File::Temp qw(tempdir);
 
-my $i = 1;
+my $i = 0;
 my $result = 0;
 my $executing_path = "$FindBin::Bin";
+my $temp_dir = tempdir(CLEANUP => 1);
 
-select STDOUT;
-print $i++ .". Checking that format_sra works correctly...";
-unless (system_call ("cp $executing_path/test_sra.fasta $executing_path/test_inst.fasta") == 0) {
+
+##########################################################################################
+## Testing format_sra.pl
+##########################################################################################
+
+print ++$i .". Checking that format_sra works correctly...";
+unless (system_call ("cp $executing_path/test_sra.fasta $temp_dir/test_inst.fasta") == 0) {
 	die "Couldn't find test_sra.fasta";
 }
 
-
-$result = system_call ("perl $executing_path/../format_sra.pl -in $executing_path/test_inst.fasta -out $executing_path/test_inst -num 7");
+$result = system_call ("perl $executing_path/../format_sra.pl -in $temp_dir/test_inst.fasta -out $temp_dir/test_inst -num 7");
 if ($result == 1) {
 	print "\nFormat_sra failed. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
 	exit;
 }
+
+$result = system_call ("tail -n +2 $temp_dir/test_inst.atram > $temp_dir/test_inst.test");
+$result = system_call ("diff $executing_path/test_atram.txt $temp_dir/test_inst.test > $executing_path/test_inst.results.$i.diff");
+if ($result == 1) {
+	print "\nFormat_sra returned incorrect results. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
+	exit;
+}
+
 print "OK\n";
 
-print $i++ . ". Checking that AlignmentPipeline works correctly...";
-$result = system_call ("perl $executing_path/../Pipelines/AlignmentPipeline.pl -samples $executing_path/testsamples.txt -targets $executing_path/testtargets.txt -out $executing_path/test_inst");
+##########################################################################################
+## Testing AlignmentPipeline.pl
+##########################################################################################
+
+print ++$i . ". Checking that AlignmentPipeline works correctly...";
+
+# make some test target files and sample files:
+open FH, ">", "$temp_dir/test_inst.samples";
+print FH "test\t$temp_dir/test_inst.atram";
+close FH;
+
+open FH, ">", "$temp_dir/test_inst.targets";
+print FH "protein\t$executing_path/protref.fasta\n";
+print FH "region\t$executing_path/testref.fasta\n";
+print FH "bad\t$executing_path/badref.fasta\n";
+print FH "complete\t$executing_path/completeref.fasta\n";
+close FH;
+
+$result = system_call ("perl $executing_path/../Pipelines/AlignmentPipeline.pl -samples $temp_dir/test_inst.samples -targets $temp_dir/test_inst.targets -out $temp_dir/test_inst");
 if ($result == 1) {
 	print "\nAlignmentPipeline died in execution. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
 	exit;
 }
 
-$result = system_call ("diff $executing_path/test_results1.txt $executing_path/test_inst/results.txt > test_inst.results1.diff");
+$result = system_call ("diff $executing_path/test_results1.txt $temp_dir/test_inst/results.txt > $executing_path/test_inst.results.$i.diff");
 if ($result == 1) {
 	print "\nAlignmentPipeline returned incorrect results. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
 	exit;
@@ -36,23 +66,28 @@ if ($result == 1) {
 
 print "OK\n";
 
-print $i++ . ". Checking that BasicPipeline works correctly...";
-$result = system_call ("perl $executing_path/../Pipelines/AlignmentPipeline.pl -samples $executing_path/testsamples.txt -targets $executing_path/testtargets.txt -out $executing_path/test_inst");
+##########################################################################################
+## Testing BasicPipeline.pl
+##########################################################################################
+
+print ++$i . ". Checking that BasicPipeline works correctly...";
+$result = system_call ("perl $executing_path/../Pipelines/BasicPipeline.pl -samples $temp_dir/test_inst.samples -targets $temp_dir/test_inst.targets -out $temp_dir/test_inst");
 if ($result == 1) {
 	print "\nBasicPipeline died in execution. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
 	exit;
 }
 
-$result = system_call ("cat $executing_path/test_inst/test/*.results.txt > $executing_path/test_inst/results.txt");
-$result = system_call ("diff $executing_path/test_results2.txt $executing_path/test_inst/results.txt > test_inst.results2.diff");
+$result = system_call ("cat $temp_dir/test_inst/test/*.results.txt > $temp_dir/test_inst/results.txt");
+$result = system_call ("diff $executing_path/test_results2.txt $temp_dir/test_inst/results.txt > $executing_path/test_inst.results.$i.diff");
 if ($result == 1) {
 	print "\nBasicPipeline returned incorrect results. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
 	exit;
 }
 print "OK\n";
 
+
 print "\nAll tests successfully passed.\n\n";
-system_call("rm -r $executing_path/test_inst*");
+system_call("rm -r $executing_path/test_inst.results.*");
 
 sub system_call {
 	my $cmd = shift;
