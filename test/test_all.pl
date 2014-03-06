452ed1441d0434463c964dc6b0e4f89be9e0205a
daisieh_local <daisieh@gmail.com>
2014-03-05 17:44:37 -0800

    Unify the calls in test_all.pl to use System.pm.

--- test/test_all.pl
+++ test/test_all.pl
@@ -5,10 +5,12 @@ use FindBin;
 use File::Temp qw(tempdir);
 use lib "$FindBin::Bin/../lib";
 use System;
+use constant ECHO => 1;
 
 my $i = 0;
 my $result = 0;
 my $debug_flag = "";
+my $log_flag = "";
 my $executing_path = "$FindBin::Bin";
 my $temp_dir = tempdir(CLEANUP => 1);
 if (@ARGV[0] eq "debug") {
@@ -19,6 +21,7 @@ if (@ARGV[0] eq "debug") {
 	close FH;
 	set_log("debug.log");
 	$debug_flag = "-debug";
+	$log_flag = "-log " . System::get_log_file();
 }
 
 $temp_dir = File::Spec->rel2abs($temp_dir);
@@ -26,9 +29,8 @@ $temp_dir = File::Spec->rel2abs($temp_dir);
 ##########################################################################################
 ## Testing format_sra.pl
 ##########################################################################################
-
-print ++$i .". Checking that format_sra works correctly...";
-$result = system_call ("perl $executing_path/../format_sra.pl -in $executing_path/test_sra.fasta -out $temp_dir/test_db -num 7 $debug_flag", 1);
+printlog (++$i .". Checking that format_sra works correctly...", ECHO);
+$result = system_call ("perl $executing_path/../format_sra.pl -in $executing_path/test_sra.fasta -out $temp_dir/test_db -num 7 $debug_flag $log_flag", 1);
 if ($result == 1) {
 	print "\nFormat_sra failed. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
 	exit;
@@ -43,12 +45,22 @@ if ($result == 1) {
 
 print "OK\n";
 
+printlog (++$i .". Checking a defective file...", ECHO);
+
+$result = system_call ("perl $executing_path/../format_sra.pl -in $executing_path/test_bad.fasta -out $temp_dir/test_db $debug_flag $log_flag", 1);
+if ($result == 0) {
+	print "Test failed. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
+	exit;
+}
+
+print "OK\n";
+
 ##########################################################################################
 ## Testing aTRAM.pl
 ##########################################################################################
 
-print ++$i .". Checking that aTRAM works correctly...";
-$result = system_call ("perl $executing_path/../aTRAM.pl -db $temp_dir/test_db -target $executing_path/testref.fasta -out $temp_dir/test_atram $debug_flag", 1);
+printlog (++$i .". Checking that aTRAM works correctly...", ECHO);
+$result = system_call ("perl $executing_path/../aTRAM.pl -db $temp_dir/test_db -target $executing_path/testref.fasta -out $temp_dir/test_atram $debug_flag $log_flag", 1);
 if ($result == 1) {
 	print "\aTRAM failed. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
 	exit;
@@ -66,7 +78,7 @@ print "OK\n";
 ## Testing AlignmentPipeline.pl
 ##########################################################################################
 
-print ++$i . ". Checking that AlignmentPipeline works correctly...";
+printlog (++$i .". Checking that AlignmentPipeline works correctly...", ECHO);
 
 # make some test target files and sample files:
 open FH, ">", "$temp_dir/test.samples";
@@ -80,7 +92,7 @@ print FH "bad\t$executing_path/badref.fasta\n";
 print FH "complete\t$executing_path/completeref.fasta\n";
 close FH;
 
-$result = system_call ("perl $executing_path/../Pipelines/AlignmentPipeline.pl -samples $temp_dir/test.samples -targets $temp_dir/test.targets -out $temp_dir/test_ap -iter 5 $debug_flag -log " . System::get_log_file());
+$result = system_call ("perl $executing_path/../Pipelines/AlignmentPipeline.pl -samples $temp_dir/test.samples -targets $temp_dir/test.targets -out $temp_dir/test_ap -iter 5 $debug_flag $log_flag");
 if ($result != 0) {
 	print "\nAlignmentPipeline died in execution. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
 	exit;
@@ -103,8 +115,8 @@ print "OK\n";
 ## Testing BasicPipeline.pl
 ##########################################################################################
 
-print ++$i . ". Checking that BasicPipeline works correctly...";
-$result = system_call ("perl $executing_path/../Pipelines/BasicPipeline.pl -samples $temp_dir/test.samples -targets $temp_dir/test.targets -out $temp_dir/test_bp -iter 5 $debug_flag -log " . System::get_log_file());
+printlog (++$i .". Checking that BasicPipeline works correctly...", ECHO);
+$result = system_call ("perl $executing_path/../Pipelines/BasicPipeline.pl -samples $temp_dir/test.samples -targets $temp_dir/test.targets -out $temp_dir/test_bp -iter 5 $debug_flag $log_flag");
 if ($result != 0) {
 	print "\nBasicPipeline died in execution. Please contact the developers with details of this failure at https://github.com/juliema/aTRAM/issues.\n";
 	exit;
