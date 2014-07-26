15b00ca94eaaaeda1c8b8faa52d3e80e65b3a8c6
daisieh_local <daisieh@gmail.com>
2014-07-25 23:56:47 -0700

    Configuration broken into subfunctions

--- lib/Configuration.pm
+++ lib/Configuration.pm
@@ -15,7 +15,7 @@ BEGIN {
 	# Inherit from Exporter to export functions and variables
 	our @ISA         = qw(Exporter);
 	# Functions and variables which are exported by default
-	our @EXPORT      = qw(get_req_software get_optional_software check_module get_assemblers get_bin);
+	our @EXPORT      = qw(get_req_software get_optional_software check_module get_assemblers get_bin get_config_file get_atram_path);
 	# Functions and variables which can be optionally exported
 	our @EXPORT_OK   = qw();
 }
@@ -26,25 +26,16 @@ my @opt_software = qw(muscle mafft);
 my $assemblers = {};
 my $assembler_dir = "";
 my $config_file = "";
+my $atrampath = "";
 
 sub initialize {
 	if (%$binaries) {
 		# we've already initialized.
 		return;
 	}
-
-	# find the lib path in @INC:
-	foreach my $path (@INC) {
-		$path = realpath ($path);
-		$path = File::Spec->canonpath($path);
-		$path =~ s/\\/\//g;
-		if ($path =~ /TRAM.*lib/i) {
-			$config_file = File::Spec->catfile($path, "config.txt");
-			$assembler_dir = File::Spec->catdir($path, "Assembler");
-			last;
-		}
-	}
-
+	$config_file = get_config_file();
+	$atrampath = get_atram_path();
+	$assembler_dir = File::Spec->catdir($atrampath, "lib", "Assembler");
 	if ((-s $config_file) > 0) {
 		open my $fh, "<:crlf", $config_file or die "Couldn't find $config_file. Did you run configure.pl?";
 		foreach my $line (<$fh>) {
@@ -56,6 +47,39 @@ sub initialize {
 			}
 		}
 	}
+}
+
+sub get_config_file {
+	if ($atrampath eq "") {
+		$atrampath = get_atram_path();
+	}
+	$config_file = File::Spec->catfile($atrampath, "config.txt");
+
+
+	return $config_file;
+}
+
+sub get_atram_path {
+	# find the atram path in @INC:
+	foreach my $path (@INC) {
+		$path = realpath ($path);
+		$path = File::Spec->canonpath($path);
+		$path =~ s/\\/\//g;
+		if ($path =~ /TRAM/i) {
+			my @pathpieces = File::Spec->splitpath($path);
+			# pop off the end bits until we're at the base aTRAM path:
+			while (@pathpieces > 0) {
+				my $lastpiece = pop @pathpieces;
+				if ($lastpiece =~ /TRAM/i) {
+					push @pathpieces, $lastpiece;
+					$atrampath = File::Spec->catdir(@pathpieces);
+					last;
+				}
+			}
+			last;
+		}
+	}
+	return $atrampath;
 }
 
 sub get_req_software {
