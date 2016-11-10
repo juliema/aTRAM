bbee0cdde1cf2b1d982d8d47a8452a3ddd7bb329
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2016-11-10 16:03:24 -0500

    Use command line arguments and better defaults

--- format_sra.py
+++ format_sra.py
@@ -1,64 +1,59 @@
-import os
 import re
+import os
 import sys
-import glob
 import time
 import sqlite3
 import argparse
+import datetime
+import functools
 import subprocess
-from multiprocessing import Pool
+import multiprocessing
 import numpy as np
 # from Bio import SeqIO  # 2-3x slower
 
 FRAGMENT = re.compile(r'^ [>@] \s* ( .* ) ( [\s\/_] [12] )', re.VERBOSE)
 
-DEFAULT_BATCH_SIZE = 10000000  # TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-DEFAULT_SHARDS = 8  # TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-DEFAULT_PROCESSES = DEFAULT_SHARDS  # TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-DB_PATH = 'data/temp_sqlite.db'  # TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+DEFAULT_BATCH_SIZE = 1e7
+LOG_FILE = None
 
 
-def timing(f):
-    def wrap(*args):
-        time1 = time.time()
-        ret = f(*args)
-        time2 = time.time()
-        print('%s function took %0.3f s' % (f.__name__, (time2 - time1)))
-        return ret
-    return wrap
+def create_log_file(args):
+    global LOG_FILE
+    log_file = '{}log.txt'.format(args.out)
+    LOG_FILE = open(log_file, 'w')
 
 
 def log(s):
-    print(s)  # TODO
+    global LOG_FILE
+    LOG_FILE.write('{}: {}\n'.format(datetime.datetime.now().isoformat(' '), s))
 
 
 def bulk_insert(db, recs):
     if recs:
-        db.executemany('''INSERT INTO ends (frag, seq, seq_end) VALUES (?, ?, ?)''', recs)
+        db.executemany('''INSERT INTO frags (frag, frag_end, seq) VALUES (?, ?, ?)''', recs)
         db.commit()
 
 
-# @timing
-def load_seqs(db, sra_files):
-    for file_name in sra_files:
+def load_seqs(db, args):
+    for file_name in args.sra_files:
         log('Loading "{}" into sqlite database'.format(file_name))
         with open(file_name, 'r') as sra_file:
-            recs, seq, end, frag, is_seq = [], '', '', 0, True
+            recs, seq, frag_end, frag, is_seq = [], '', '', 0, True
             for line in sra_file:
                 if not line:
                     pass
                 elif line[0] in ['>', '@']:
                     if seq:
-                        recs.append((frag, seq, end))
+                        recs.append((frag, frag_end, seq))
                     seq = ''
                     match = FRAGMENT.match(line)
                     is_seq = True
                     if match:
                         frag = match.group(1)
-                        end = match.group(2)
+                        frag_end = match.group(2)
                     else:
                         frag = line[1:]
-                        end = ''
+                        frag_end = ''
                 elif line[0] == '+':
                     is_seq = False
                 elif line[0].isalpha() and is_seq:
@@ -68,22 +63,24 @@ def load_seqs(db, sra_files):
                     recs = []
             else:
                 if seq:
-                    recs.append((frag, seq, end))
+                    recs.append((frag, frag_end, seq))
                 bulk_insert(db, recs)
 
 
 def create_table(db):
     log('Creating sqlite tables')
-    db.execute('''CREATE TABLE IF NOT EXISTS ends (frag TEXT, seq TEXT, seq_end TEXT)''')
+    db.execute('''DROP INDEX IF EXISTS frag''')
+    db.execute('''DROP TABLE IF EXISTS frags''')
+    db.execute('''CREATE TABLE IF NOT EXISTS frags (frag TEXT, frag_end TEXT, seq TEXT)''')
 
 
-# @timing
 def create_index(db):
     log('Creating sqlite indices')
-    db.execute('''CREATE INDEX IF NOT EXISTS frag ON ends (frag)''')
+    db.execute('''CREATE INDEX IF NOT EXISTS frag ON frags (frag)''')
 
 
-def connect_db(db_path):
+def connect_db(args):
+    db_path = '{}sqlite.db'.format(args.out)
     db = sqlite3.connect(db_path)
     db.execute("PRAGMA page_size = {}".format(2**16))
     db.execute("PRAGMA journal_mode = 'off'")
@@ -91,13 +88,13 @@ def connect_db(db_path):
     return db
 
 
-def assign_seqs_to_shards(db, shard_count):
+def assign_seqs_to_shards(db, args):
     log('Assigning sequences to shards')
-    connection = db.execute('SELECT COUNT(*) FROM ends')
+    connection = db.execute('SELECT COUNT(*) FROM frags')
     total = connection.fetchone()[0]
-    offsets = np.linspace(0, total, num=shard_count + 1, dtype=int)
+    offsets = np.linspace(0, total, num=args.shards + 1, dtype=int)
     for i in range(1, len(offsets) - 1):
-        connection = db.execute('SELECT frag FROM ends ORDER BY frag LIMIT 2 OFFSET {}'.format(offsets[i]))
+        connection = db.execute('SELECT frag FROM frags ORDER BY frag LIMIT 2 OFFSET {}'.format(offsets[i]))
         first = connection.fetchone()[0]
         second = connection.fetchone()[0]
         if first != second:
@@ -107,13 +104,13 @@ def assign_seqs_to_shards(db, shard_count):
     return list(zip(limits, offsets[:-1]))
 
 
-def create_blast_db(shard, i):
-    db = connect_db(DB_PATH)
-    sql = 'SELECT frag, seq_end, seq FROM ends ORDER BY frag LIMIT {} OFFSET {}'.format(shard[0], shard[1])
+def create_blast_db(args, shard, i):
+    db = connect_db(args)
+    sql = 'SELECT frag, frag_end, seq FROM frags ORDER BY frag LIMIT {} OFFSET {}'.format(shard[0], shard[1])
     connection = db.execute(sql)
-    fasta_file = 'data/temp_seqs_{}.fasta'.format(i)
-    blast_prefix = 'data/blast_{}'.format(i)
-    with open(fasta_file, 'w') as out_file:  # TODO
+    fasta_file = '{}temp_seqs_{}.format'.format(args.out, i)
+    blast_prefix = '{}blast_{}'.format(args.out, i)
+    with open(fasta_file, 'w') as out_file:
         for row in connection:
             out_file.write('>{}{}\n'.format(row[0], row[1]))
             out_file.write('{}\n'.format(row[2]))
@@ -123,11 +120,11 @@ def create_blast_db(shard, i):
     os.remove(fasta_file)
 
 
-def create_blast_dbs(process_count, shards):
+def create_blast_dbs(args, shards):
     log('Making blast DBs')
-    with Pool(processes=process_count) as pool:
+    with multiprocessing.Pool(processes=args.processes) as pool:
         for i, shard in enumerate(shards):
-            proc = pool.Process(target=create_blast_db, args=(shard, i))
+            proc = pool.Process(target=create_blast_db, args=(args, shard, i))
             proc.start()
     pool.join()
 
@@ -136,32 +133,39 @@ def parse_args():
     parser = argparse.ArgumentParser(description='''
         Takes fasta or fastq files of paired-end (or single-end) short reads and creates an aTRAM database.
     ''')
-    parser.add_argument('sra_files', nargs='+', help='short read archive in fasta or fastq format.')
-    parser.add_argument('-o', '--out', help='output aTRAM files with this prefix')
-    parser.add_argument('-l', '--log', help='save log information to this file')
+    parser.add_argument('sra_files', nargs='+', help='short read archives in fasta or fastq format. May contain wildcards.')
+    parser.add_argument('-o', '--out', help='output aTRAM files with this prefix. May include a directory in the prefix.')
     parser.add_argument('-s', '--shards', type=int, help='number of shards to create')
     parser.add_argument('-p', '--processes', type=int, help='number of processes to create')
     args = parser.parse_args()
 
-    args.shards = args.shards if args.shards else DEFAULT_SHARDS
-    args.processes = args.processes if args.processes else DEFAULT_PROCESSES
+    size = 0
+    for f in args.sra_files:
+        size += os.path.getsize(f) / 2 if f.lower().endswith('.fastq') else os.path.getsize(f)
+    size = int(size / 2.5e8)
+    default_shards = size if size else 1
+    args.shards = args.shards if args.shards else default_shards
 
-    # TODO sra_files must exist and be in fasta/q format
-    # TODO sra_files should be globs
+    default_processes = os.cpu_count() - 2 if os.cpu_count() > 2 else 1
+    args.processes = args.processes if args.processes else default_processes
 
     return args
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parse_args()
 
-    db = connect_db(DB_PATH)  # TODO: make based on args
+    create_log_file(args)
+    log(' '.join(sys.argv))
+
+    db = connect_db(args)
 
     create_table(db)
-    load_seqs(db, args.sra_files)
+    load_seqs(db, args)
     create_index(db)
 
-    shards = assign_seqs_to_shards(db, args.shards)
-    create_blast_dbs(args.processes, shards)
+    shards = assign_seqs_to_shards(db, args)
+    create_blast_dbs(args, shards)
 
     db.close()
+    LOG_FILE.close()
