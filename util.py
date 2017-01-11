791bcf56ccf7c38857f9231a79bf472797c31a51
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2017-01-11 18:40:33 -0500

    WIP filtering contigs

--- util.py
+++ util.py
@@ -21,7 +21,7 @@ def log_setup(config):
     logging.info(' '.join(sys.argv))
 
 
-def get_blast_shards(config):
+def get_shard_file_names(config):
     """Get all of the BLAST DB names built by format_sra."""
     file_name = '{}blast_*.nhr'.format(config['file_prefix'])
     pattern = os.path.join(config['work_dir'], file_name)
@@ -29,47 +29,54 @@ def get_blast_shards(config):
     return sorted([f[:-4] for f in files])
 
 
-def db_file(config):
-    """Create a standard DB name used across all programs."""
-    file_name = '{}sqlite.db'.format(config['file_prefix'])
+def path(config, file_name):
+    """Standardize file names with a work directory and a file prefix."""
+    file_name = config['file_prefix'] + file_name
     return os.path.join(config['work_dir'], file_name)
+
+
+def db_file(config):
+    """Create an SQL DB name."""
+    file_name = 'sqlite.db'
+    return path(config, file_name)
 
 
 def blast_shard_file(config, iteration):
     """Standardize the BLAST shard DB names."""
-    file_name = '{}blast_{}'.format(config['file_prefix'], str(iteration + 1).zfill(3))
-    return os.path.join(config['work_dir'], file_name)
+    file_name = 'blast_{}'.format(str(iteration).zfill(2))
+    return path(config, file_name)
+
+
+def blast_contig_file(config, iteration):
+    """Get the name of the blast result file."""
+    file_name = 'contig_scores_{}.txt'.format(str(iteration).zfill(2))
+    return path(config, file_name)
+
+
+def contig_score_file(config, iteration):
+    """Get the name of the blast DB for the assembled contigs."""
+    file_name = 'blast_contigs_{}'.format(str(iteration).zfill(2))
+    return path(config, file_name)
+
+
+def paired_end_file(config, iteration, end):
+    """Standardize the name of the paired file."""
+    file_name = 'matching_seqs_{}_{}.fasta'.format(str(iteration).zfill(2), end)
+    return path(config, file_name)
+
+
+def raw_contig_file(config, iteration):
+    """Standardize the contig file name from before it is filtered."""
+    file_name = 'raw_contigs_{}.fasta'.format(str(iteration).zfill(2))
+    return path(config, file_name)
+
+
+def contig_file(config, iteration):
+    """Standardize the contig file name from after it is filtered."""
+    file_name = 'contigs_{}.fasta'.format(str(iteration).zfill(2))
+    return path(config, file_name)
 
 
 def blast_result_file(shard, iteration):
     """Get the name of the blast result file."""
     return '{}_{}.txt'.format(shard, str(iteration).zfill(2))
-
-
-def blast_contig_file(config, iteration):
-    """Get the name of the blast result file."""
-    return '{}contig_scores_{}.txt'.format(config['file_prefix'], str(iteration).zfill(2))
-
-
-def contig_score_file(config, iteration):
-    """Get the name of the blast DB for the assembled contigs."""
-    file_name = '{}blast_contigs_{}'.format(config['file_prefix'], str(iteration + 1).zfill(3))
-    return os.path.join(config['work_dir'], file_name)
-
-
-def paired_end_file(config, iteration, end):
-    """Standardize the name of the paired file."""
-    file_name = '{}matching_seqs_{}_{}.fasta'.format(config['file_prefix'], iteration, end)
-    return os.path.join(config['work_dir'], file_name)
-
-
-def raw_contig_file(config, iteration):
-    """Standardize the contig file name from before it is filtered."""
-    file_name = '{}raw_contigs_{}.fasta'.format(config['file_prefix'], iteration)
-    return os.path.join(config['work_dir'], file_name)
-
-
-def contig_file(config, iteration):
-    """Standardize the contig file name from after it is filtered."""
-    file_name = '{}contigs_{}.fasta'.format(config['file_prefix'], iteration)
-    return os.path.join(config['work_dir'], file_name)
