3fdff4842d2ecd077a75304e1c4eded1d3f38a95
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2017-01-27 11:42:35 -0500

    More decoupling of modules

--- atram.py
+++ atram.py
@@ -48,10 +48,10 @@ def blast_sra(config, iteration, shards, target):
         _ = [result.get() for result in results]
 
 
-def connect_db(config):
+def connect_db(work_dir, file_prefix):
     """Setup the DB for our processing needs."""
 
-    db_path = util.db_file(config)
+    db_path = util.db_file(work_dir, file_prefix)
     db_conn = sqlite3.connect(db_path)
     return db_conn
 
@@ -70,15 +70,15 @@ def get_matching_fragments(iteration, shards):
     return frags
 
 
-def write_sequences(config, iteration, fragments):
+def write_sequences(work_dir, file_prefix, iteration, fragments):
     """
     Take the matching blast hits and write the sequence and any matching end to
     the appropriate fasta files.
     """
 
-    db_conn = connect_db(config)
-    fasta_1 = util.paired_end_file(config, iteration, '1')
-    fasta_2 = util.paired_end_file(config, iteration, '2')
+    db_conn = connect_db(work_dir, file_prefix)
+    fasta_1 = util.paired_end_file(work_dir, file_prefix, iteration, '1')
+    fasta_2 = util.paired_end_file(work_dir, file_prefix, iteration, '2')
     paired = False
     with open(fasta_1, 'w') as file_1, open(fasta_2, 'w') as file_2:
         for ids in chunked(fragments.keys(), 100):
@@ -95,31 +95,32 @@ def write_sequences(config, iteration, fragments):
     return paired
 
 
-def create_blast_db(config, iteration):
+def create_blast_db(work_dir, file_prefix, iteration):
     """Create a blast DB from the assembled fragments."""
 
-    blast_db = util.contig_score_db(config, iteration)
-    fasta_file = util.contig_unfiltered_file(config, iteration)
+    blast_db = util.contig_score_db(work_dir, file_prefix, iteration)
+    fasta_file = util.contig_unfiltered_file(work_dir, file_prefix, iteration)
     cmd = 'makeblastdb -dbtype nucl -in {} -out {}'
     cmd = cmd.format(fasta_file, blast_db)
     subprocess.check_call(cmd, shell=True)
     return blast_db
 
 
-def blast_target_against_contigs(config, iteration):
+def blast_target_against_contigs(work_dir, file_prefix, iteration,
+                                 target, genetic_code, protein):
     """
     Blast the target sequence against the contings. The blast output will have
     the scores for later processing.
     """
 
-    blast_db = create_blast_db(config, iteration)
-    scored_contigs = util.contig_score_file(config, iteration)
+    blast_db = create_blast_db(work_dir, file_prefix, iteration)
+    scored_contigs = util.contig_score_file(work_dir, file_prefix, iteration)
     blast_args = dict(
-        db=blast_db, query=config['target'], out=scored_contigs,
+        db=blast_db, query=target, out=scored_contigs,
         outfmt="'10 qseqid sseqid bitscore qstart qend sstart send slen'")
-    if config['protein']:
+    if protein:
         cmd = str(NcbitblastnCommandline(
-            cmd='tblastn', db_gencode=config['genetic_code'], **blast_args))
+            cmd='tblastn', db_gencode=genetic_code, **blast_args))
     else:
         cmd = str(NcbiblastnCommandline(
             cmd='blastn', task='blastn', **blast_args))
@@ -127,20 +128,21 @@ def blast_target_against_contigs(config, iteration):
     subprocess.check_call(cmd, shell=True)
 
 
-def filter_contig_scores(config, iteration):
+def filter_contig_scores(work_dir, file_prefix, iteration, bit_score):
     """Remove contigs that have scores below the bit score cut-off."""
 
     # qseqid sseqid bitscore qstart qend sstart send slen
     field_names = ['target_id', 'contig_id', 'bit_score',
                    'target_start', 'target_end',
                    'contig_start', 'contig_end', 'contig_len']
-    contig_score_file = util.contig_score_file(config, iteration)
+    contig_score_file = util.contig_score_file(work_dir, file_prefix,
+                                               iteration)
 
     scores = {}
     with open(contig_score_file) as in_file:
         for score in csv.DictReader(in_file, field_names):
             score['bit_score'] = float(score['bit_score'])
-            if score['bit_score'] >= config['bit_score']:
+            if score['bit_score'] >= bit_score:
                 for field in field_names[3:]:
                     score[field] = int(score[field])
                 scores[score['contig_id']] = score
@@ -148,13 +150,15 @@ def filter_contig_scores(config, iteration):
     return scores
 
 
-def filter_contigs(config, iteration):
+def filter_contigs(work_dir, file_prefix, iteration):
     """Remove junk from the assembled contigs."""
 
-    blast_target_against_contigs(config, iteration)
-    filtered_names = filter_contig_scores(config, iteration)
-    unfiltered_file = util.contig_unfiltered_file(config, iteration)
-    # filtered_file = util.contig_filtered_file(config, iteration)
+    blast_target_against_contigs(work_dir, file_prefix, iteration)
+    filtered_names = filter_contig_scores(work_dir, file_prefix, iteration)
+    unfiltered_file = util.contig_unfiltered_file(work_dir, file_prefix,
+                                                  iteration)
+    # filtered_file = util.contig_filtered_file(work_dir, file_prefix,
+    #                                           iteration)
 
     print(filtered_names)
     with open(unfiltered_file) as in_file:
@@ -181,10 +185,10 @@ def atram(config):
 
 
 if __name__ == '__main__':
-    ARGS = configure.parse_command_line(
+    CONFIG = configure.parse_command_line(
         description=""" """,
         args=('target protein iterations cpu evalue max_target_seqs '
               'assembler max_memory file_prefix work_dir bit_score '
               'genetic_code kmer'))
-    util.log_setup(ARGS)
-    atram(ARGS)
+    util.log_setup(CONFIG.work_dir, CONFIG.file_prefix)
+    atram(CONFIG)
