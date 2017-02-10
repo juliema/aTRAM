a2c2989b7f1b64a504ec84205978bc2ba0e8dfed
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2017-02-10 13:02:38 -0500

    Older pythons don't like to pickle static methods

--- atram_preprocessor.py
+++ atram_preprocessor.py
@@ -27,7 +27,7 @@ class AtramPreprocessor:
         self.db_conn = None
         self.shard_list = None
 
-    def preprocess(self):
+    def run(self):
         """The main method."""
 
         self.config = Configure().parse_command_line(
@@ -56,7 +56,7 @@ class AtramPreprocessor:
         filer = Filer(self.config.work_dir, self.config.file_prefix)
         filer.log_setup()
 
-        self.db_conn = self.connect_db(filer)
+        self.db_conn = connect_db(filer)
         self.create_table()
         self.load_seqs()
         self.create_index()
@@ -66,32 +66,13 @@ class AtramPreprocessor:
 
         self.db_conn.close()
 
-    @staticmethod
-    def connect_db(filer):
-        """
-        Setup the DB for our processing needs and return a DB connection.
-
-        Because this is called in a child process, the address space is not
-        shared with the parent (caller) hence we cannot use instance variables.
-        """
-
-        db_path = filer.db_file_name()
-        db_conn = sqlite3.connect(db_path)
-        db_conn.execute("PRAGMA page_size = {}".format(2**16))
-        db_conn.execute("PRAGMA journal_mode = 'off'")
-        db_conn.execute("PRAGMA synchronous = 'off'")
-        return db_conn
-
     def create_table(self):
         """Reset the DB. Delete the tables and recreate them."""
 
         logging.info('Creating sqlite tables')
         self.db_conn.execute('''DROP INDEX IF EXISTS seq_names''')
         self.db_conn.execute('''DROP TABLE IF EXISTS sequences''')
-        sql = '''
-            CREATE TABLE IF NOT EXISTS sequences
-            (seq_name TEXT, seq_end TEXT, seq TEXT)
-            '''
+        sql = 'CREATE TABLE sequences (seq_name TEXT, seq_end TEXT, seq TEXT)'
         self.db_conn.execute(sql)
 
     def create_index(self):
@@ -101,9 +82,7 @@ class AtramPreprocessor:
         """
 
         logging.info('Creating sqlite indices')
-        self.db_conn.execute('''
-            CREATE INDEX IF NOT EXISTS seq_names ON sequences (seq_name)
-            ''')
+        self.db_conn.execute('CREATE INDEX seq_names ON sequences (seq_name)')
 
     def load_seqs(self):
         """
@@ -235,57 +214,74 @@ class AtramPreprocessor:
             results = []
             for shard_index, shard_params in enumerate(self.shard_list):
                 results.append(pool.apply_async(
-                    self.create_blast_db, (config.work_dir, config.file_prefix,
-                                           shard_params, shard_index)))
+                    create_blast_db,
+                    (config.work_dir, config.file_prefix,
+                     shard_params, shard_index)))
 
             _ = [result.get() for result in results]
 
         logging.info('Finished making blast DBs')
 
-    @staticmethod
-    def create_blast_db(work_dir, file_prefix, shard_params, shard_index):
-        """
-        Create a blast DB from the shard. We fill a fasta file with the
-        appropriate sequences and hand things off to the makeblastdb program.
 
-        Because this is called in a child process, the address space is not
-        shared with the parent (caller) hence we cannot use instance variables.
-        """
+def create_blast_db(work_dir, file_prefix, shard_params, shard_index):
+    """
+    Create a blast DB from the shard. We fill a fasta file with the
+    appropriate sequences and hand things off to the makeblastdb program.
 
-        filer = Filer(work_dir=work_dir, file_prefix=file_prefix)
-        blast_db = filer.blast_shard_name(shard_index)
+    Because this is called in a child process, the address space is not
+    shared with the parent (caller) hence we cannot use instance variables.
+    """
 
-        with filer.temp_file() as fasta_file:
-            AtramPreprocessor.fill_blast_fasta(fasta_file, filer, shard_params)
+    filer = Filer(work_dir=work_dir, file_prefix=file_prefix)
+    blast_db = filer.blast_shard_name(shard_index)
 
-            cmd = 'makeblastdb -dbtype nucl -in {} -out {}'
-            cmd = cmd.format(fasta_file.name, blast_db)
-            subprocess.check_call(cmd, shell=True)
+    with filer.temp_file() as fasta_file:
+        fill_blast_fasta(fasta_file, filer, shard_params)
 
-    @staticmethod
-    def fill_blast_fasta(fasta_file, filer, shard_params):
-        """
-        Fill the fasta file used as input into blast with shard sequences from
-        the sqlite3 DB. We use the shard partitions passed in to determine
-        which sequences to get for this shard.
+        cmd = 'makeblastdb -dbtype nucl -in {} -out {}'
+        cmd = cmd.format(fasta_file.name, blast_db)
+        subprocess.check_call(cmd, shell=True)
 
-        Because this is called in a child process, the address space is not
-        shared with the parent (caller) hence we cannot use instance variables.
-        """
 
-        db_conn = AtramPreprocessor.connect_db(filer)
-        sql = '''
-            SELECT seq_name, seq_end, seq FROM sequences
-            ORDER BY seq_name LIMIT {} OFFSET {}
-            '''
-        sql = sql.format(shard_params[0], shard_params[1])
-        result = db_conn.execute(sql)
-        for row in result:
-            fasta_file.write('>{}{}\n'.format(row[0], row[1]))
-            fasta_file.write('{}\n'.format(row[2]))
-        db_conn.close()
+def connect_db(filer):
+    """
+    Setup the DB for our processing needs and return a DB connection.
+
+    Because this is called in a child process, the address space is not
+    shared with the parent (caller) hence we cannot use instance variables.
+    """
+
+    db_path = filer.db_file_name()
+    db_conn = sqlite3.connect(db_path)
+    db_conn.execute("PRAGMA page_size = {}".format(2**16))
+    db_conn.execute("PRAGMA journal_mode = 'off'")
+    db_conn.execute("PRAGMA synchronous = 'off'")
+    return db_conn
+
+
+def fill_blast_fasta(fasta_file, filer, shard_params):
+    """
+    Fill the fasta file used as input into blast with shard sequences from
+    the sqlite3 DB. We use the shard partitions passed in to determine
+    which sequences to get for this shard.
+
+    Because this is called in a child process, the address space is not
+    shared with the parent (caller) hence we cannot use instance variables.
+    """
+
+    db_conn = connect_db(filer)
+    sql = '''
+        SELECT seq_name, seq_end, seq FROM sequences
+        ORDER BY seq_name LIMIT {} OFFSET {}
+        '''
+    sql = sql.format(shard_params[0], shard_params[1])
+    result = db_conn.execute(sql)
+    for row in result:
+        fasta_file.write('>{}{}\n'.format(row[0], row[1]))
+        fasta_file.write('{}\n'.format(row[2]))
+    db_conn.close()
 
 
 if __name__ == '__main__':
 
-    AtramPreprocessor().preprocess()
+    AtramPreprocessor().run()
