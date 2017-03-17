efed6f0eff6bf67ab0ff8da2d19c7ea83434b01c
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2017-03-17 14:39:45 -0400

    WIP atram early exit checks

--- lib/bio.py
+++ lib/bio.py
@@ -1,9 +1,20 @@
 """Utilities for working with sequences."""
 
+import re
+
 COMPLEMENT = str.maketrans('ACGTUWSMKRYBDHVNXacgtuwsmkrybdhvnx',
                            'TGCAAWSKMYRVHDBNXtgcaawskmyrvhdbnx')
+
+IS_PROTEIN = re.compile(r'[EFILPQ]', re.IGNORECASE)
 
 
 def reverse_complement(seq):
     """Reverse complement a nucleotide squence. We added some wildcards."""
+
     return seq.translate(COMPLEMENT)[::-1]
+
+
+def is_protein(seq):
+    """A simple to test to see if the sequence is a protein."""
+
+    return IS_PROTEIN.search(seq)
