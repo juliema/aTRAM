f6af643c42aad39db73a18a812d06694223bd0fe
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2017-10-11 18:11:41 -0400

    Updating tests

--- tests/test_atram.py
+++ tests/test_atram.py
@@ -18,6 +18,7 @@ class TestAtram(unittest.TestCase):
         self.args = {
             'query': ['query_file_1', 'query_file_2'],
             'blast_db': ['blast_db_1', 'blast_db_2'],
+            'iterations': 1,
             'log_file': 'log_file_1',
             'temp_dir': 'temp_dir_1'}
         self.assembler = BaseAssembler(self.args, self.db_conn)
@@ -138,101 +139,100 @@ class TestAtram(unittest.TestCase):
         create_contig_blast_hits_table.assert_called_once_with(dbh)
         create_sra_blast_hits_table.assert_called_once_with(dbh)
 
-    # def test_assembly_loop_one_iter(self):
-    #     db_conn = st.text()
-    #     blast_db = st.text()
-    #     query = st.text()
-    #     iter_dir = st.text()
-    #     args = {'temp_dir': st.text(), 'iterations': 1}
-    #
-    #     assembler = BaseAssembler(args, db_conn)
-    #     assembler.blast_only = False
-    #     assembler.state['query_file'] = query
-    #     assembler.state['blast_db'] = blast_db
-    #
-    #     mock.it(log, 'info')
-    #     mock.it(assembler, 'initialize_iteration')
-    #     mock.it(assembler, 'iter_dir', iter_dir)
-    #     mock.it(os, 'makedirs')
-    #     mock.it(atram, 'blast_query_against_all_shards')
-    #     mock.it(assembler, 'no_blast_hits', False)
-    #     mock.it(assembler, 'write_input_files')
-    #     mock.it(assembler, 'run')
-    #     mock.it(assembler, 'nothing_assembled', False)
-    #     mock.it(atram, 'filter_contigs', 99)
-    #     mock.it(assembler, 'assembled_contigs_count', 11)
-    #     mock.it(assembler, 'no_new_contigs', False)
-    #     mock.it(atram, 'create_query_from_contigs', 'my_query')
-    #
-    #     atram.assembly_loop(assembler, blast_db, query)
-    #
-    #     expect = [{'msg': 'aTRAM blast DB = "{}", query = "{}", '
-    #                       'iteration {}'.format(blast_db, query, 1)},
-    #               {'msg': 'All iterations completed'}]
-    #     assert expect == mock.filter('lib.log', 'info')
-    #
-    #     expect = [
-    #         {'blast_db': blast_db, 'iteration': 1, 'query_file': query}]
-    #     assert expect == mock.filter('BaseAssembler', 'initialize_iteration')
-    #
-    #     assert [{}] == mock.filter('BaseAssembler', 'iter_dir')
-    #
-    #     expect = [{'name': iter_dir, 'exist_ok': True}]
-    #     assert expect == mock.filter('os', 'makedirs')
-    #
-    #     expect = [{'assembler': assembler}]
-    #     assert expect == mock.filter('atram', 'blast_query_against_all_shards')
-    #
-    #     assert [{}] == mock.filter('BaseAssembler', 'no_blast_hits')
-    #     assert [{}] == mock.filter('BaseAssembler', 'write_input_files')
-    #     assert [{}] == mock.filter('BaseAssembler', 'run')
-    #     assert [{}] == mock.filter('BaseAssembler', 'nothing_assembled')
-    #     assert [{'count': 11}] == mock.filter('BaseAssembler', 'no_new_contigs')
-    #
-    #     expect = [{'assembler': assembler}]
-    #     assert expect == mock.filter('atram', 'create_query_from_contigs')
-    #
-    # # def test_assembly_loop_multiple_iterations(self):
-    #
-    # # def test_assembly_loop_blast_only(self):
-    #
-    # # def test_assembly_loop_no_blast_hits(self):
-    #
-    # # def test_assembly_loop_nothing_assembled(self):
-    #
-    # # def test_assembly_loop_no_assembled_contigs_count(self):
-    #
-    # # def test_assembly_loop_no_new_contigs(self):
-    #
-    # def test_shard_fraction_one():
-    #     args = {'fraction': 1.0}
-    #     returns = ['1st', '2nd', '3rd', '4th']
-    #     assembler = BaseAssembler(args, 'db_conn')
-    #     assembler.state['blast_db'] = 'my_blast_db'
-    #
-    #     mock.it(blast, 'all_shard_paths', returns=[returns])
-    #
-    #     shards = atram.shard_fraction(assembler)
-    #
-    #     assert returns == shards
-    #
-    #     expect = [{'blast_db': 'my_blast_db'}]
-    #     assert expect == mock.filter('lib.blast', 'all_shard_paths')
-    #
-    # def test_shard_fraction_half(self):
-    #     args = {'fraction': 0.5}
-    #     assembler = BaseAssembler(args, 'db_conn')
-    #     assembler.state['blast_db'] = 'my_blast_db'
-    #
-    #     mock.it(blast, 'all_shard_paths', returns=[['1st', '2nd', '3rd', '4th']])
-    #
-    #     shards = atram.shard_fraction(assembler)
-    #
-    #     assert ['1st', '2nd'] == shards
-    #
-    #     expect = [{'blast_db': 'my_blast_db'}]
-    #     assert expect == mock.filter('lib.blast', 'all_shard_paths')
-    #
+    @patch('lib.log.info')
+    @patch('os.makedirs')
+    @patch('atram.blast_query_against_all_shards')
+    @patch('atram.create_query_from_contigs')
+    @patch('atram.filter_contigs')
+    def test_assembly_loop_one_iter(
+            self,
+            filter_contigs,
+            create_query_from_contigs,
+            blast_query_against_all_shards,
+            b_makedirs,
+            info):
+        iter_dir = 'my_iter_dir'
+
+        self.assembler.blast_only = False
+        self.assembler.state['query_file'] = self.args['query'][0]
+        self.assembler.state['blast_db'] = self.args['blast_db'][0]
+        self.assembler.run = MagicMock()
+        self.assembler.write_input_files = MagicMock()
+        self.assembler.initialize_iteration = MagicMock()
+        self.assembler.iter_dir = MagicMock(return_value=iter_dir)
+        self.assembler.no_blast_hits = MagicMock(return_value=False)
+        self.assembler.no_new_contigs = MagicMock(return_value=False)
+        self.assembler.nothing_assembled = MagicMock(return_value=False)
+        self.assembler.assembled_contigs_count = MagicMock(return_value=11)
+
+        atram.assembly_loop(
+            self.assembler, self.args['blast_db'][0], self.args['query'][0])
+
+        calls = [
+            call('aTRAM blast DB = "{}", query = "{}", iteration {}'.format(
+                self.args['blast_db'][0], self.args['query'][0], 1)),
+            call('All iterations completed')]
+        info.assert_has_calls(calls)
+
+        self.assembler.initialize_iteration.assert_called_once_with(
+            self.args['blast_db'][0], self.args['query'][0], 1)
+
+        self.assembler.iter_dir.assert_called_once()
+
+        b_makedirs.assert_called_once_with(iter_dir, exist_ok=True)
+
+        blast_query_against_all_shards.assert_called_once_with(self.assembler)
+
+        self.assembler.no_blast_hits.assert_called_once()
+        self.assembler.no_blast_hits.write_input_files()
+        self.assembler.no_blast_hits.run()
+        self.assembler.no_blast_hits.nothing_assembled()
+
+        self.assembler.no_new_contigs.assert_called_once_with(11)
+
+        create_query_from_contigs.create_query_from_contigs(self.assembler)
+        filter_contigs.create_query_from_contigs(self.assembler)
+
+    # def test_assembly_loop_multiple_iterations(self):
+
+    # def test_assembly_loop_blast_only(self):
+
+    # def test_assembly_loop_no_blast_hits(self):
+
+    # def test_assembly_loop_nothing_assembled(self):
+
+    # def test_assembly_loop_no_assembled_contigs_count(self):
+
+    # def test_assembly_loop_no_new_contigs(self):
+
+    @patch('lib.blast.all_shard_paths')
+    def test_shard_fraction_one(self, all_shard_paths):
+        returns = ['1st', '2nd', '3rd', '4th']
+        self.assembler.state['blast_db'] = self.args['blast_db'][0]
+        self.assembler.args['fraction'] = 1.0
+
+        all_shard_paths.return_value = returns
+
+        shards = atram.shard_fraction(self.assembler)
+
+        assert returns == shards
+
+        all_shard_paths.assert_called_once_with(self.args['blast_db'][0])
+
+    @patch('lib.blast.all_shard_paths')
+    def test_shard_fraction_half(self, all_shard_paths):
+        self.assembler.args['fraction'] = 0.5
+        self.assembler.state['blast_db'] = self.args['blast_db'][0]
+        returns = ['1st', '2nd', '3rd', '4th']
+
+        all_shard_paths.return_value = returns
+
+        shards = atram.shard_fraction(self.assembler)
+
+        assert ['1st', '2nd'] == shards
+
+        all_shard_paths.assert_called_once_with(self.args['blast_db'][0])
+
     # # def test_blast_query_against_one_shard(self):
     #
     # # def test_filter_contigs(self):
