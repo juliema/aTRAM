55387591bbfe205009647724b6a27c2534a9cb39
raphael.lafrance@gmail.com <raphael.lafrance@gmail.com>
2018-07-27 10:36:51 -0400

    Bump version

--- lib/db.py
+++ lib/db.py
@@ -6,7 +6,7 @@ import os
 from os.path import basename, join, exists
 
 
-ATRAM_VERSION = '2.0.alpha.6'
+ATRAM_VERSION = 'v2.0.alpha.6'
 DB_VERSION = '2.0'
 
 BATCH_SIZE = 1e6  # How many sequence records to insert at a time
