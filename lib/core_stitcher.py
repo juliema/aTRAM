c8b99a700e5c85c4a2de1666ead25ca2be249538
rafe <raphael.lafrance@gmail.com>
2019-04-05 17:00:14 -0400

    Main loop to run exonerate

--- lib/core_stitcher.py
+++ lib/core_stitcher.py
@@ -4,15 +4,17 @@ Stitch together exons from targeted assemblies.
 It uses amino acid targets and DNA assemblies.
 """
 
-import re
 import os
-from os.path import join, split, splitext
+from os.path import abspath, join, split, splitext
+import re
+import subprocess
 from glob import glob
-# import subprocess
+
 from Bio.SeqIO.FastaIO import SimpleFastaParser
-# import lib.db as db
+
 import lib.log as log
 import lib.util as util
+# import lib.db as db
 
 
 class Sticher:
@@ -35,124 +37,182 @@ class Sticher:
 
             self.prepare_fasta_files()
             self.prepare_reference_genes()
-            # stitcher.find_fasta_files_for_genes()
-            # stitcher.run_exonerate()
+            self.find_fasta_files_for_genes()
+            self.run_exonerate()
+            self.get_gontigs()
+            # self.stitch_contigs()
+            # self.summary_stats()
 
     def prepare_fasta_files(self):
-        """Remove characters exonerate cannot handle from assembly names."""
-        idx = 0
+        """Prepare fasta files for exonerate."""
         pattern = join(self.args['assemblies_dir'], '*.fasta')
-        for fasta_in_path in sorted(glob(pattern)):
-            if os.stat(fasta_in_path).st_size == 0:
+
+        for fasta_old_path in sorted(glob(pattern)):
+            if os.stat(fasta_old_path).st_size == 0:
                 continue
 
-            log.info('Preparing fasta file: {}'.format(fasta_in_path))
+            log.info('Preparing fasta file: {}'.format(fasta_old_path))
 
-            _, file_part = split(fasta_in_path)
-            file_part, _ = splitext(file_part)
-            fasta_out_path = join(self.temp_dir, '{}.ed.fasta'.format(file_part))
+            fasta_new_path = self.new_fasta_name(fasta_old_path)
+            tiebreaker = 0
 
-            with open(fasta_in_path) as fasta_in, \
-                    open(fasta_out_path, 'w') as fasta_out:
-                for seq_name, seq in SimpleFastaParser(fasta_in):
-                    idx += 1
+            with open(fasta_old_path) as fasta_old, \
+                    open(fasta_new_path, 'w') as fasta_new:
 
+                for seq_name, seq in SimpleFastaParser(fasta_old):
+                    tiebreaker += 1
                     seq_name = seq_name.strip()
                     seq_name = re.sub(r'[\s,=@+\[\]:!-]+', '_', seq_name)
-                    seq_name = '{}_{}'.format(seq_name, idx)
+                    seq_name = '{}_{}'.format(seq_name, tiebreaker)
 
-                    util.write_fasta_record(fasta_out, seq_name, seq)
+                    util.write_fasta_record(fasta_new, seq_name, seq)
 
     def prepare_reference_genes(self):
-        """Format reference sequences for exonerate."""
+        """
+        Format reference sequences for exonerate.
+
+        1) Touch up the sequence name.
+        2) Put each sequence into its own file.
+        3) Save a list of all of the reference gene files in a master list.
+        """
         ref_genes = self.args['reference_genes']
 
         log.info('Preparing reference genes: {}'.format(ref_genes))
 
         with open(ref_genes) as ref_in, \
-                open(self.gene_list_path, 'w') as ref_list:
+                open(self.all_ref_genes_path, 'w') as all_ref_genes_file:
+
             for seq_name, seq in SimpleFastaParser(ref_in):
                 seq_name = re.sub(r'\s+', '_', seq_name)
 
-                ref_file = self.ref_file(seq_name)
+                ref_gene_path = self.ref_gene_file_path(seq_name)
 
-                ref_list.write(ref_file)
-                ref_list.write('\n')
+                all_ref_genes_file.write(ref_gene_path)
+                all_ref_genes_file.write('\n')
 
-                with open(ref_file, 'w') as ref_out:
-                    util.write_fasta_record(ref_out, seq_name, seq)
+                with open(ref_gene_path, 'w') as ref_gene_file:
+                    util.write_fasta_record(ref_gene_file, seq_name, seq)
 
     def find_fasta_files_for_genes(self):
-        """Find genes in the gene list."""
-        with open(self.gene_list_path) as gene_list_files:
-            for gene_list_file in gene_list_files:
-                gene_list_file = gene_list_file.strip()
-                _, gene = split(gene_list_file)
-                gene = gene.split('.')[0]
-                log.info('Finding fasta files for gene {}'.format(gene))
-                with open(gene_list_file, 'w') as file_for_gene:
-                    print(file_for_gene)
-                    pattern = join(self.temp_dir, '*{}*.ed.fasta'.format(gene))
-                    fasta_list = glob(pattern)
-                    print(fasta_list)
-                    for fasta in fasta_list:
-                        file_for_gene.write(fasta)
-                        file_for_gene.write('\n')
+        """
+        Find all assemblies for every gene in the gene list.
 
-    # def run_exonerate(self):
-    #     """
-    #     Run exonerate on every reference sequence, taxon combination.
-    #
-    #     Get exonerate information for each exon.
-    #     Then sort the results file by taxon and by location of the exon.
-    #     Use exonerate to pull out the exons.
-    #     """
-    #     taxa_list = self.args['taxa_list']
-    #     with open(gene_list_path) as gene_file, open(taxa_list) as taxa_file:
-    #         for gene in gene_file:
-    #             gene = gene.strip()
-    #             taxa_file.seek(0)
-    #             for taxon in taxa_file:
-    #                 taxon = taxon.strip()
-    #                 log.info('{} {}'.format(gene, taxon))
-    #                 gene_asm_path = join(self.temp_dir, '{}.list.txt'.format(gene))
-    #                 with open(gene_asm_path) as gene_asm_file:
-    #                     for asm in gene_asm_file:
-    #                         asm = asm.strip()
-    #                         self.exonerate_cmds(
-    #                             gene_list_path, gene, taxon, asm)
-    #
-    # def exonerate_cmds(self, ref_file, gene, taxon, asm):
-    #     """Build and run the exonerate program."""
-    #     results_file = join(self.temp_dir, '{}.results.csv'.format(gene))
-    #     cmd = (
-    #         'exonerate --verbose 0 --model protein2genome {ref_file} {asm} '
-    #         '--showvulgar no --showalignment no '
-    #         '--ryo "{gene},{taxon},%ql,%qal,%qab,%qae,%ti\n" >> {results_file};'
-    #     ).format(ref_file=ref_file, asm=asm, gene=gene, taxon=taxon,
-    #              results_file=results_file)
-    #     subprocess.check_call(cmd, shell=True)
-    #
-    #     exons_file = join(self.temp_dir, '{}.exons.csv'.format(gene))
-    #     cmd = (
-    #         'exonerate --verbose 0 --model protein2genome {ref_file} {asm} '
-    #         '--showvulgar no --showalignment no '
-    #         '--ryo ">{taxon},%ti,%qab\n%tcs\n" >> {exons_file}; '
-    #     ).format(ref_file=ref_file, asm=asm, taxon=taxon, exons_file=exons_file)
-    #     subprocess.check_call(cmd, shell=True)
-    #
-    #     sorted_file = join(self.temp_dir, '{}.results.sorted.csv'.format(gene))
-    #     cmd = (
-    #         'LC_ALL=C sort -t, -k 1,1d -k 2,2d -k 5,5d  '
-    #         '{results_file} > {sorted_file};'
-    #     ).format(results_file=results_file, sorted_file=sorted_file)
-    #     subprocess.check_call(cmd, shell=True)
+        Put this list into file for the gene. One file listing all assemblies
+        for every gene in the master gene list.
+        """
+        with open(self.all_ref_genes_path) as all_ref_genes_file:
+            for ref_gene in all_ref_genes_file:
+                gene_name = self.gene_name_from_ref_gene(ref_gene)
+
+                log.info('Finding fasta files for gene: {}'.format(gene_name))
+
+                with open(self.gene_list_path(gene_name), 'w') as gene_list:
+                    pattern = join(
+                        self.temp_dir, '*{}*.ed.fasta'.format(gene_name))
+                    fasta_list = glob(pattern)
+
+                    for fasta in fasta_list:
+                        gene_list.write(fasta)
+                        gene_list.write('\n')
+
+    def run_exonerate(self):
+        """
+        Run exonerate on every reference sequence, taxon combination.
+
+        Get exonerate information for each exon.
+        Then sort the results file by taxon and by location of the exon.
+        Use exonerate to pull out the exons.
+        """
+        with open(self.all_ref_genes_path) as ref_genes_file:
+            ref_genes = {x.strip() for x in ref_genes_file}
+
+        with open(self.args['taxa_list']) as taxa_list:
+            taxa = {x.strip() for x in taxa_list}
+
+        for ref_gene in sorted(ref_genes):
+            gene_name = self.gene_name_from_ref_gene(ref_gene)
+            log.info('Reference file: {}'.format(gene_name))
+            gene_asm_path = self.gene_list_path(gene_name)
+            with open(gene_asm_path) as gene_asm_file:
+                asms = [asm.strip() for asm in gene_asm_file
+                        if self.taxon_from_asm(gene_name, taxa, asm) in taxa]
+                for asm in sorted(asms):
+                    taxon = self.taxon_from_asm(gene_name, taxa, asm)
+                    self.exonerate_cmds(ref_gene, gene_name, taxon, asm)
+
+    def exonerate_cmds(self, ref_file, gene_name, taxon, asm):
+        """Build and run the exonerate program."""
+        results_file = self.results_file_path(gene_name)
+        cmd = (
+            'exonerate --verbose 0 --model protein2genome {ref_file} {asm} '
+            '--showvulgar no --showalignment no '
+            r'--ryo "{gene},{taxon},%ql,%qal,%qab,%qae,%ti\n" '
+            '>> {results_file};'
+        ).format(ref_file=ref_file, asm=asm, gene=gene_name, taxon=taxon,
+                 results_file=results_file)
+        subprocess.check_call(cmd, shell=True)
+
+        exons_file = self.exons_file_path(gene_name)
+        cmd = (
+            'exonerate --verbose 0 --model protein2genome {ref_file} {asm} '
+            '--showvulgar no --showalignment no '
+            r'--ryo ">{taxon},%ti,%qab\n%tcs\n" >> {exons_file}; '
+        ).format(ref_file=ref_file, asm=asm, taxon=taxon,
+                 exons_file=exons_file)
+        subprocess.check_call(cmd, shell=True)
+
+        sorted_file = self.sorted_file_path(gene_name)
+        cmd = (
+            'LC_ALL=C sort -t, -k 1,1d -k 2,2d -k 5,5d  '
+            '{results_file} > {sorted_file};'
+        ).format(results_file=results_file, sorted_file=sorted_file)
+        subprocess.check_call(cmd, shell=True)
 
     @property
-    def gene_list_path(self):
-        """Return gene list file."""
+    def all_ref_genes_path(self):
+        """Return the file that holds the list of all reference gene files."""
         return join(self.temp_dir, 'Gene_list.txt')
 
-    def ref_file(self, seq_name):
-        """Build reference file name."""
+    def ref_gene_file_path(self, seq_name):
+        """Build reference gene file name from the sequence name."""
         return join(self.temp_dir, '{}.reference.fasta'.format(seq_name))
+
+    def gene_list_path(self, gene_name):
+        """Build file name with list of assemblies for a reference gene."""
+        return join(self.temp_dir, '{}.list.txt'.format(gene_name))
+
+    def results_file_path(self, gene_name):
+        """Build the file name for the exonerate results."""
+        results_file = join(self.temp_dir, '{}.results.csv'.format(gene_name))
+        return abspath(results_file)
+
+    def exons_file_path(self, gene_name):
+        """Build the file name for the exonerate exons."""
+        exons_file = join(self.temp_dir, '{}.exons.fasta'.format(gene_name))
+        return abspath(exons_file)
+
+    def sorted_file_path(self, gene_name):
+        """Build the file name for the exonerate sorted results."""
+        sorted_file = join(
+            self.temp_dir, '{}.results.sorted.csv'.format(gene_name))
+        return abspath(sorted_file)
+
+    @staticmethod
+    def gene_name_from_ref_gene(ref_file):
+        """Get the gene name from the reference gene file path."""
+        ref_file = ref_file.strip()
+        _, gene_name = split(ref_file)
+        return re.sub(r'\.reference\.fasta$', '', gene_name)
+
+    def new_fasta_name(self, old_fasta_name):
+        """Get the new fasta assembly file name from the old one."""
+        _, file_part = split(old_fasta_name)
+        file_part, _ = splitext(file_part)
+        return join(self.temp_dir, '{}.ed.fasta'.format(file_part))
+
+    @staticmethod
+    def taxon_from_asm(gene_name, taxa, asm):
+        """Check if the assembly in the taxa set."""
+        _, asm = split(asm)
+        asm = re.sub(r'^{}\.'.format(gene_name), '', asm)
+        return re.sub(r'\.ed\.fasta\s*$', '', asm)
