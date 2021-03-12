73cced43e33c2f2b5fc9ade90e614627e8d670b0
rafe <raphael.lafrance@gmail.com>
2021-03-12 10:54:36 -0500

    Tag a new release (2.4.0)

--- lib/db.py
+++ lib/db.py
@@ -5,7 +5,7 @@ import sqlite3
 import sys
 from os.path import basename, exists, join
 
-ATRAM_VERSION = 'v2.3.5'
+ATRAM_VERSION = 'v2.4.0'
 
 # DB_VERSION != ATRAM_VERSION
 # We don't force DB changes until required.
